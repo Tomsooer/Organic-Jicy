295a939f64d903714ae0fe429eecb2a8ce5cbc9e
Tomsooer <tsoer2013@gmail.com>
2025-06-28 17:03:26 +0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuIcon = document.querySelector('.menu-icon');
-console.log(menuIcon);
+header = document.querySelector('header');
+	body = document.querySelector(body);
 
 menuIcon.addEventListener('click', () => {
 	menuIcon.classList.toggle('menu-icon--active');
+	header.classList.toggle('header--mobile');
+	body.classList.toggle('no-scroll')
 });
